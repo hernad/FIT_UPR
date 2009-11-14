9f1415d2b04af05d01efbac8b0431064598f67bb
Ernad Husremovic <hernad@bring.out.ba>
2009-11-14 20:18:12 +0100

    zadatak 12

---
+++ zadaci/12/z_12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string tab = "\t";
+
+	cout << "Ime:"  << tab << tab << "Ismet" << endl;
+	cout << "Prezime:" << tab << "Maksumić" << endl;
+	cout << "Zaposlen:" << tab << "FIT" << endl;
+	
+
+}
