581c9e2ebd668b466e053c82372889a62a1304f0
Ernad Husremovic <hernad@bring.out.ba>
2009-11-14 20:43:13 +0100

    sizeoff

---
+++ vjezbe/03/sizeoff_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "Velicina int-a je:\t"<< sizeof(int)<< "bajta\n";
+cout << "Velicina short int-a je:\t"<<sizeof(short)<<"bajta\n";
+cout << "Velicina long int-a je:\t"<< sizeof(long)<< "bajta\n";
+cout << "Velicina char-a je:\t"<< sizeof(char)<< "bajta\n";
+cout << "Velicina float-a je:\t"<< sizeof(float)<< "bajta\n";
+cout << "Velicina double-a je:\t"<< sizeof(double) <<"bajta\n";
+
+return 0;
+}
+
+
