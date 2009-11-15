410de0036653308d503527a1e35cfed70e18b1cd
Ernad Husremovic <hernad@bring.out.ba>
2009-11-15 14:14:20 +0100

    str -> char

---
+++ forumi/str_char.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string str_var = "ABC";
+ 
+        cout << str_var[0] + 1;
+        
+	// ovaka casting nije moguc;
+        // cout << char(str_var);
+
+
+	cout << endl;
+
+	return 0;
+}
