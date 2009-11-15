470cf16904ff2d76fbf446595373eb0d06f9bd68
Ernad Husremovic <hernad@bring.out.ba>
2009-11-15 15:00:08 +0100

    implicit casting

---
+++ zadaci/22/z_22.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+       char a1, a2;
+       a1 = '@';
+       a2 = 64;
+       cout << "a1 = " << a1 << endl;
+       cout << "a2 = " << a2 << endl;
+       return 0;
+}
+
