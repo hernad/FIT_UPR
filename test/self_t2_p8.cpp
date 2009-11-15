6c4a68afdf50c38b3a25eb35dfb116d6b9c658f7
Ernad Husremovic <hernad@bring.out.ba>
2009-11-15 16:40:41 +0100

    pitanje 8 - navlakusa sa indentacijom

---
+++ test/self_t2_p8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+int Broj = 4;
+float alfa = -1.0;
+
+if (Broj>0)
+	if(alfa>0)
+		cout<<"Prvi";
+	else
+		cout << "Drugi";
+
+cout << "Treci";
+
+return 0;
+
+}
