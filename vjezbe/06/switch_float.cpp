5a27877776f01c18b28981c6697673840458772a
Ernad Husremovic <hernad@bring.out.ba>
2009-11-15 01:06:34 +0100

    switch float - err, int

---
+++ vjezbe/06/switch_float.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	float f=7.7;
+
+	switch (f) {
+		case f > 5:
+			cout << "f>5";
+		otherwise:
+			cout << "otherwise";
+	
+ 	}
+	
+	return 0;
+}
---
+++ vjezbe/06/switch_int.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int i_1 = 8;
+
+	switch (i_1) {
+		case 5:
+			cout << "i_1 = 5" << endl;
+			break;
+		case 8:
+			cout << "i_1 = 8" << endl;
+			break;
+		default:
+			cout << "default" << endl;
+	
+ 	}
+	
+	return 0;
+}
