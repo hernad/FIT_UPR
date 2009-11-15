2dbf7727aaf07acf20a26277a34f687e8580e3f1
Ernad Husremovic <hernad@bring.out.ba>
2009-11-15 16:54:50 +0100

    return u void funkciji

---
+++ test/self_t2_p14.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+
+	float pi = 3.14
+	float r = 2;
+	cout << "Pi =" << pi << "," << "\n" <<
+             " a povrsina kruga iznosi";
+
+	cout << r * r * pi << "1" << "endl";
+
+	return 0;
+}
