034c295c671d1493fefd170bf2fc5d06a1324dd1
Ernad Husremovic <hernad@bring.out.ba>
2009-11-15 15:05:41 +0100

    char1 + char2 = int

---
+++ zadaci/25/z_25.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+       cout << "@ + # = " << char('@' + '#') << " ; " << '@' + '#' << endl;
+       cout << "Ovaj program zna računati!" << char(2) << endl;
+       return 0;
+}
+
