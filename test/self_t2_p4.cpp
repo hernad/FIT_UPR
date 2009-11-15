c2b62cb6d6fa662da69a4f52ef9ea80c4c6140b5
Ernad Husremovic <hernad@bring.out.ba>
2009-11-15 16:49:15 +0100

    p4 ajoj ... corav pored ociju

---
+++ test/self_t2_p4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+int B = 10;
+
+int A;
+
+char C = 'c';
+
+A = B + C;
+
+if (A==C)
+	cout << A << endl;
+else
+	cout << C;
+
+return 0;
+
+}
